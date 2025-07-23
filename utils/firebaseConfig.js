54519984526aaace28c96cd1629a5ed3a9bf4ce4
Nikhil Kandhare <98297220+1900690105@users.noreply.github.com>
2025-07-24 05:08:44 +0530

    Update firebaseConfig.js

--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3aS-7twBFch6Dbu_Frkk-vWDghunt-A0",
-  authDomain: "filexerox-f6623.firebaseapp.com",
-  projectId: "filexerox-f6623",
-  storageBucket: "filexerox-f6623.firebasestorage.app",
-  messagingSenderId: "723025570382",
-  appId: "1:723025570382:web:9f43c2b725dfa25bb0311f",
-  measurementId: "G-8EWW7V7Q00",
+ //config
 };
 
 // Initialize Firebase
